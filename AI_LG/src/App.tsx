373b0a2d3506f87167494a8c03cecce462cb1cd6
Kevin Valenzuelauela <kevintores92@gmail.com>
2025-12-22 13:58:27 +0800

    node update

--- AI_LG/src/App.tsx
+++ AI_LG/src/App.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Landing from "@/pages/Landing";
-import SignUp from "@/pages/SignUp";
+import SignUp from "@/pages/signup";
 import BusinessInfo from "@/pages/BusinessInfo";
 import UploadList from "@/pages/UploadList";
 import CampaignNew from "@/pages/CampaignNew";
